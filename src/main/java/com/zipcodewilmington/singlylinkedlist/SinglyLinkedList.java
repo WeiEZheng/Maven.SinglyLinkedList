6bee1cdbc357de99af6d29606666d25aa354104f
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-03-27 14:37:09 -0400

    created node and methods for add, get, size

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,63 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList <T> {
+    private LinkedListNode<T> head;
+    private int listSize;
+
+    public static class LinkedListNode<T> {
+        private T currentData;
+        private LinkedListNode nextData;
+
+        public LinkedListNode(T currentData){
+            this.currentData = currentData;
+            this.nextData = null;
+        }
+
+        public void setNext(LinkedListNode<T> next){
+            nextData = next;
+        }
+
+        public LinkedListNode<T> getNext(){
+            return nextData;
+        }
+
+        public T getData(){
+            return currentData;
+        }
+    }
+
+    public void add(T element){
+        LinkedListNode<T> thisNode = new LinkedListNode<T>(element);
+        if (head==null)
+            head=thisNode;
+        else {
+            LinkedListNode<T> next = head;
+            while (next.getNext() != null) {
+                next = next.getNext();
+            }
+            next.setNext(thisNode);
+        }
+        listSize++;
+    }
+
+    public T get(int index){
+        int count=0;
+        LinkedListNode<T> current = head;
+        while (count!=index){
+            current=current.getNext();
+            count++;
+        }
+        return current.getData();
+    }
+
+    public int getSize() {
+        return listSize;
+    }
+
+    //	- remove -- remove an element (specified by numeric index) from the list
+    //	- contains -- returns true if the element is in the list, false otherwise
+    //	- find -- returns the element's index if it is in the list, -1 otherwise
+    //	- copy -- returns a new linked list containing the same values (look up deep versus shallow copy)
+    //	- sort -- sorts the list using your algorithm of choice. You must perform the sorting yourself (no fair using someone else's library)
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,27 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @Test
+    void linkNodeTest(){
+        SinglyLinkedList.LinkedListNode linkedListNode = new SinglyLinkedList.LinkedListNode(1);
+        SinglyLinkedList.LinkedListNode linkedListNode1 = new SinglyLinkedList.LinkedListNode(2);
+        linkedListNode.setNext(linkedListNode1);
+        Assertions.assertEquals(linkedListNode1, linkedListNode.getNext());
+        Assertions.assertNull(linkedListNode1.getNext());
+        Assertions.assertEquals(2,linkedListNode1.getData());
+    }
+    @Test
+    void linkListAdd(){
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.add(1);
+        singlyLinkedList.add(2);
+        Assertions.assertEquals(1,singlyLinkedList.get(0));
+        Assertions.assertEquals(2, singlyLinkedList.getSize());
+    }
 }
